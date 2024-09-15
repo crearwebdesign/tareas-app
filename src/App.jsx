adced7c54b788d1d9ba81c17d69ad986379468cd
crearwebdesign <oscar10034@gmail.com>
2024-09-15 17:58:19 -0400

    pantalla blanca

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TaskForm from "./components/TaskForm";
 
 function App() {
   return (
-    <main className="bg-zinc-900 h-screen">
+    <main className="bg-white h-screen">
       <div className="container mx-auto p-10">
         <TaskForm />
         <Task />
